ffa1bd99798b96f8d18b778a0ff2ad7de9b2ac88
fangyzh26 <103894715+fangyzh26@users.noreply.github.com>
2023-08-09 20:34:43 +0800

    Update sram.v

--- apb_sram/sram.v
+++ apb_sram/sram.v
@@ -6,18 +6,15 @@ module sram(
     input [9:0]         addr,
     input [31:0]        din,
 
-    // output reg          ready,
     output reg [31:0]   dout
     );
 
     integer i;
-    // reg delay_factor;
 
     reg [31:0] mem [31:0]; //dinfine a ram, depth=32, data width=32bit
 
     always @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
-            // delay_factor <= 'd0;
             for (i=0; i<=31; i=i+1) begin
                 mem[i] <= 'b0;
             end
@@ -27,11 +24,6 @@ module sram(
                 mem[addr]   <= din; // write data
             else 
                 dout        <= mem[addr]; //read data
-            // #(40*delay_factor) ready = 1;
-            // delay_factor = delay_factor + 1;
-            // //just to simulate a ready signal
-            // //actually, using sram ip, do not use this pattern, which can't be synthesized
-            // #21 ready = 0;
         end
     end
     
